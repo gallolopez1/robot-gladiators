60f516de97db5b8e00ae88fd23134a4087fbdbf3
Rooster <gallolopez1@hotmail.com>
2021-10-11 16:12:42 -0400

    added and executed functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is you robot's name?")
     // Note the lack of quotation marks around playerName
 console.log(playerName)
+
 console.log("This logs a string, good for leaving yourself a message");
 // this will do math and log 20
 console.log(10 + 10);
